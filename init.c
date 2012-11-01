6a09db64eb185b29f6113f57817980366bf2a7bf
Russell Hofmann <russell@rphil.de>
2012-11-01 18:17:21 +0100

    clear screen at the beginning

--- init.c
+++ init.c
@@ -4,6 +4,9 @@ void init(void)
 	int i;
 	char* video = (char*) 0xb8000;
 
+	for(video+=4000; video !=(char*)0xb8000; video--)
+		*video=0;
+
 	for (i = 0; hw[i] != '\0'; i++) {
 
 		video[i*2] = hw[i];
