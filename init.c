d8d50e6f62373ab4a2b3243e69f9d0defc5e027d
Russell Hofmann <russell@rphil.de>
2012-11-01 16:01:50 +0100

    created init.c

---
+++ init.c
@@ -0,0 +1,14 @@
+void init(void)
+{
+	const char hw[] = "Hello World!";
+	int i;
+	char* video = (char*) 0xb8000;
+
+	for (i = 0; hw[i] != '\0'; i++) {
+
+		video[i*2] = hw[i];
+
+		// 0x07 = Hellgrau auf Schwarz
+		video[i * 2 + 1] = 0x07;
+	}
+}
